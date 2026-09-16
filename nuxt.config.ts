00b08029bb538df7047d9f3da548716021127e88
潘静 <>
2026-09-16 09:29:44 +0800

    修改变量

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
-  runtimeConfig: {
-    // 服务端私有变量（不会暴露给客户端）
-    databaseUrl: process.env.DATABASE_URL,
-    zhipuApiKey: process.env.ZHIPU_API_KEY,
-  },
+
 })
